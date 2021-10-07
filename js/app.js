2cfd59494f16a22444280b9d5bc9110ccbc4c795
AGs-hue <agnesarong@gmail.com>
2021-10-07 15:17:08 +0100

    update repo

--- js/app.js
+++ js/app.js
@@ -9,21 +9,32 @@ const rand = Math.floor(Math.random() * 20) + 1;
 console.log("we good");
 
 // // number of guess trial
-let trial = 5;
-
+let trial = 0;
+// alert('15' === 15)
 function checkGuess() {
+
   let myGuess = guess.value;
-  while (trial < 5) {
-    if (myGuess === rand) {
+  if (trial < 5) {
+  
+    trial++
+    if (myGuess == rand) {
       feedback.textContent = ("Yaay Correct!");
+      submitGuess.removeEventListener('click', checkGuess);
+
     } else if (myGuess > rand) {
       feedback.textContent = ("Yikes! " + myGuess + "Too high, Try Again!");
     } else if (myGuess < rand) {
       feedback.textContent = ("Yikes! " + myGuess + "Too low, Try Again!");
     }
+    else {
+      alert('hello world');
+    }
   }
+if(trial >= 5) {
+  feedback.textContent = ('You have exceeded number of trial');
 }
-submitGuess.addEventListener('onclick', checkGuess);
+}
+submitGuess.addEventListener('click', checkGuess);
 console.log('working') 
 
 
